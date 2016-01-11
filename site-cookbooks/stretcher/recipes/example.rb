790712a86d26b3267de936ab7b44a6c5f4d2c36a
tjinjin <tjinjinprogram@gmail.com>
2016-01-12 01:59:33 +0900

    Faster exec example app

--- site-cookbooks/stretcher/recipes/example.rb
+++ site-cookbooks/stretcher/recipes/example.rb
@@ -9,9 +9,20 @@
 package 'git'
 package 'golang'
 
+include_recipe 'consul'
+
 git "/tmp/stretcher" do
   user 'vagrant'
   group 'vagrant'
   repository "https://github.com/fujiwara/stretcher.git"
   action :sync
+end
+
+execute 'initialize sample' do
+  command <<-EOC
+    cd /tmp/stretcher/examples
+    sed -i 's/^\(consul event\)/#\1/g' prepare.sh
+    ./prepare.sh
+  EOC
+  not_if { File.exist?('/tmp/stretcher/examples/example.tar.gz')}
 end
