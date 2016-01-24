40af8a0f81f7dbd51b80cd87d7a89d469fda3a5e
tjinjin <tjinjinprogram@gmail.com>
2016-01-24 22:00:18 +0900

    Add stretcher chef

---
+++ config/deploy.rb
@@ -0,0 +1,9 @@
+role :build, ['192.168.34.42'], :no_release => true
+set :application, 'try-stretcher'
+set :deploy_to, '~/chef-solo/try-stretcher'
+set :deploy_roles, 'web'
+set :stretcher_hooks, 'config/stretcher.yml.erb'
+set :local_tarball_name, 'rails-applicaiton.tar.gz'
+set :stretcher_src, "s3://tjinjin-server-stretcher/assets/rails-application-#{env.now}.tgz"
+set :manifest_path, "s3://tjinjin-server-stretcher/manifests"
+set :repo_url, 'https://github.com/tjinjin/try-stretcher'
