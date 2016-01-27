2fb203c7870c233cfa85e69529c388d2e5bda852
tjinjin <tjinjinprogram@gmail.com>
2016-01-28 01:57:55 +0900

    Add leader_addr every stage

--- site-cookbooks/consul/attributes/default.rb
+++ site-cookbooks/consul/attributes/default.rb
@@ -1,2 +1,3 @@
 default['consul']['role'] = 'deploy_web_development'
 default['consul']['ipaddress'] = nil
+default['consul']['leader_addr'] = nil
--- site-cookbooks/consul/recipes/setting.rb
+++ site-cookbooks/consul/recipes/setting.rb
@@ -4,6 +4,7 @@ template '/etc/consul.d/server.json' do
   variables(
     bind_addr: node['consul']['ipaddress'],
     node_name: node['consul']['ipaddress'],
+    leader_addr: node['consul']['leader_addr'],
     environment: node['environment']
   )
   notifies :start, 'service[consul]'
