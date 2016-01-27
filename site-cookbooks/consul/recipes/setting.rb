511c1e169a1e5e12dc7bccbbe9dd42409cb94e4a
tjinjin <tjinjinprogram@gmail.com>
2016-01-28 01:50:52 +0900

    Use bind_addr and so on, when development only

--- site-cookbooks/consul/recipes/setting.rb
+++ site-cookbooks/consul/recipes/setting.rb
@@ -3,7 +3,8 @@ directory '/etc/consul.d'
 template '/etc/consul.d/server.json' do
   variables(
     bind_addr: node['consul']['ipaddress'],
-    node_name: node['consul']['ipaddress']
+    node_name: node['consul']['ipaddress'],
+    environment: node['environment']
   )
   notifies :start, 'service[consul]'
   notifies :reload, 'service[consul]'
