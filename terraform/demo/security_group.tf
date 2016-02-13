39db8a520bb144310b4c9ce9bb26bed8fa30f1f0
tjinjin <tjinjinprogram@gmail.com>
2016-02-14 01:48:51 +0900

    Add security_group

---
+++ terraform/demo/security_group.tf
@@ -0,0 +1,64 @@
+resource "aws_security_group" "bastion" {
+    name = "bastion"
+    description = "bastion sg"
+    vpc_id = "${aws_vpc.demo.id}"
+
+    ingress {
+        from_port = 0
+        to_port = 65535
+        protocol = "tcp"
+        cidr_blocks = ["10.0.0.0/16"]
+    }
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+      tags {
+          Name = "demo"
+          Created = "terraform"
+      }
+}
+
+resource "aws_security_group" "private_instances" {
+    name = "private_instances"
+    description = "belong to private ec2 instances"
+    vpc_id = "${aws_vpc.demo.id}"
+
+    ingress {
+        from_port = 0
+        to_port = 65535
+        protocol = "tcp"
+        cidr_blocks = ["10.0.0.0/16"]
+    }
+
+    # for consul
+    ingress {
+        from_port = 8301
+        to_port = 8302
+        protocol = "udp"
+        cidr_blocks = ["10.0.0.0/16"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags {
+        Name = "demo"
+        Created = "terraform"
+    }
+}
