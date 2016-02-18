34c148312bb2291870be8ce3170c2bd5024fd8a0
tjinjin <tjinjinprogram@gmail.com>
2016-02-19 02:19:18 +0900

    consul-kv-dashboard

--- terraform/demo/security_group.tf
+++ terraform/demo/security_group.tf
@@ -23,6 +23,13 @@ resource "aws_security_group" "bastion" {
         cidr_blocks = ["0.0.0.0/0"]
     }
 
+    ingress {
+        from_port = 3000
+        to_port = 3000
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
     egress {
         from_port = 0
         to_port = 0
