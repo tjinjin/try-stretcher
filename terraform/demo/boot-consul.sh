cf17d4dffdf3340ae7d72d2f619ecb72bdb7e326
tjinjin <tjinjinprogram@gmail.com>
2016-03-02 00:45:26 +0900

    Fix

--- terraform/demo/boot-consul.sh
+++ terraform/demo/boot-consul.sh
@@ -11,5 +11,3 @@ systemctl start consul
 export AWS_DEFAULT_REGION=ap-northeast-1
 # chef
 echo 's3://tjinjin-server-stretcher/manifests/manifest_bastion_development.yml' | stretcher
-# rails
-echo 's3://tjinjin-server-stretcher/manifests/manifest_bastion_development.yml' | stretcher
