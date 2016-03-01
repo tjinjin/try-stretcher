fd7e806bcfe8006aca7b465743106595882d25d4
tjinjin <tjinjinprogram@gmail.com>
2016-03-02 00:10:55 +0900

    Add init task

--- terraform/demo/boot-consul.sh
+++ terraform/demo/boot-consul.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
 systemctl start consul
+
+###
+#
+# deploy tasks
+#
+###
+# for stretcher
+export AWS_DEFAULT_REGION=ap-northeast-1
+# chef
+echo 's3://tjinjin-server-stretcher/manifests/manifest_bastion_development.yml' | stretcher
+# rails
+echo 's3://tjinjin-server-stretcher/manifests/manifest_bastion_development.yml' | stretcher
