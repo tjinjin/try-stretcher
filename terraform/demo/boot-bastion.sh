d2a40e0acbc3f3b6705a3b75ec7adbb679797862
tjinjin <tjinjinprogram@gmail.com>
2016-03-12 21:37:03 +0900

    Add consul settings into bastion node

--- terraform/demo/boot-bastion.sh
+++ terraform/demo/boot-bastion.sh
@@ -1,5 +1,23 @@
 #!/bin/sh
 
+cat << EOF >/etc/consul.d/server.json
+{
+  "server": true,
+  "client_addr": "0.0.0.0",
+  "bootstrap_expect": 3,
+  "data_dir": "/tmp/consul",
+  "rejoin_after_leave": true,
+  "start_join": ["${bastion_private_ip}"],
+  "watches": [
+    {
+      "type": "event",
+      "name": "deploy_bastion_development",
+      "handler": "stretcher"
+    }
+  ]
+}
+EOF
+
 systemctl start consul
 
 ###
